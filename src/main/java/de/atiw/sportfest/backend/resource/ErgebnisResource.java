c00f021b74be59e1efc889ac771b602a2a2d9ea4
steffen.baehr <steffen.baehr@Lab0321.s-atiw.de>
2017-06-20 11:54:52 +0200

    #8 ErgebnisResource

---
+++ src/main/java/de/atiw/sportfest/backend/resource/ErgebnisResource.java
@@ -0,0 +1,36 @@
+package de.atiw.sportfest.backend.resource;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+
+@Path("/ergebnisse")
+public class ErgebnisResource {
+
+	@GET
+	@Path("/{did}")
+    public String getErgebnis(String did){
+        return "GET Ergebnis";
+    }
+	
+		
+	@POST
+	@Path("/{did}/{eid}")
+	public String postErgenis(String did, String eid){
+		return "PUT Ergebnis";
+	}
+	
+	@PUT
+	@Path("/{did}")
+	 public String putErgebnis(String did){
+        return "GET Ergebnis";
+    }
+	
+	@DELETE
+	@Path("/{did}")
+	 public String deleteErgebnis(String eid){
+        return "DELETE Ergebnis";
+    }
+}
