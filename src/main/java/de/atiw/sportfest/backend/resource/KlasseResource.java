71866be1f1e6932545875707c4e94d798363eed4
steffen.baehr <steffen.baehr@Lab0321.s-atiw.de>
2017-06-20 12:40:54 +0200

    #9 KlasseResource

--- src/main/java/de/atiw/sportfest/backend/resource/KlasseResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/KlasseResource.java
@@ -4,14 +4,15 @@ package de.atiw.sportfest.backend.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/klasse")
 public class KlasseResource {
 
 	@GET
 	@Path("/{kid}")
-    public String getKlasse(String kid){
-        return "GET Klasse(kid)";
+    public String getKlasse(@PathParam("kid") String kid){
+		return "Die Klassenbezeichnung ist: "+kid;
     }
 	
 	@GET
