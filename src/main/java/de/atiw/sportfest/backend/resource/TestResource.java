b10963673c5cb83125c859903750d3b706c3485c
Tobias Daum <tobias.daum@s-atiw>
2017-06-20 12:08:22 +0200

    #2 Test Endpoint

--- src/main/java/de/atiw/sportfest/backend/resource/TestResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/TestResource.java
@@ -1,6 +1,7 @@
 package de.atiw.sportfest.backend.resource;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 @Path("/test")
@@ -8,8 +9,25 @@ public class TestResource {
 
     @GET
     public String getPlainText(){
-        return "Hello world!";
+        return "get worked";
     }
+    
+    @POST
+    public String postTest(){
+    	return "post worked";
+    }
+    
+    @GET
+    public String dummyData(){
+    	return "dummy data";
+    }
+    
+    
+    @POST
+    public String postDummy(){
+    	return "dummy non persistant save";
+    }
+    
 
 }
 
