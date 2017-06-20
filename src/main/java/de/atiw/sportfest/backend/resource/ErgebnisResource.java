726fbf8592a91eb7b0caef3f110da5bbe4d51e8e
steffen.baehr <steffen.baehr@Lab0321.s-atiw.de>
2017-06-20 12:40:54 +0200

    #8 ErgebnisResource

--- src/main/java/de/atiw/sportfest/backend/resource/ErgebnisResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/ErgebnisResource.java
@@ -1,36 +1,38 @@
 package de.atiw.sportfest.backend.resource;
 
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/ergebnis")
 public class ErgebnisResource {
 
 	@GET
 	@Path("/{did}")
-    public String getErgebnis(String did){
-        return "GET Ergebnis";
+    public String getErgebnis(@PathParam("did") String did){
+        return "Die gewählte Disziplin ist: "+did;
     }
 	
 		
 	@POST
 	@Path("/{did}/{eid}")
-	public String postErgenis(String did, String eid){
-		return "PUT Ergebnis";
+	public String postErgenis(@PathParam("did") String did, @PathParam("eid") String eid){
+		return "PUT "+did+", "+eid;
 	}
 	
 	@PUT
 	@Path("/{did}")
-	 public String putErgebnis(String did){
-        return "GET Ergebnis";
+	 public String putErgebnis(@PathParam("did") String did){
+		return "PUT  "+ did;
     }
 	
 	@DELETE
 	@Path("/{did}")
-	 public String deleteErgebnis(String eid){
-        return "DELETE Ergebnis";
+	 public String deleteErgebnis(@PathParam("eid") String eid){
+		return "DELETE "+ eid;
     }
 }
