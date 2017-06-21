9ef410eef2c4cb206e2b67ee82f537313a8b464c
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-06-21 21:42:38 +0200

    DataSource injection test

--- src/main/java/de/atiw/sportfest/backend/resource/TestResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/TestResource.java
@@ -1,10 +1,22 @@
 package de.atiw.sportfest.backend.resource;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/test")
 public class TestResource {
+
+    @Resource(name="jdbc/sportfest")
+    DataSource db;
+
+    @GET
+    @Path("cdi")
+    public String getStatus(){
+       return db != null ? "CDI works!" : "CDI is broken!";
+    }
 
     @GET
     public String getPlainText(){
