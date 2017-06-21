038ad1f33e10823c39ebf9b2868881e969a0725d
christian.meinl <christian.meinl@Lab0320.s-atiw.de>
2017-06-21 15:30:05 +0200

    disziplin resource

--- src/main/java/de/atiw/sportfest/backend/resource/DisziplinResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/DisziplinResource.java
@@ -1,14 +1,24 @@
 package de.atiw.sportfest.backend.resource;
 
+import java.sql.Connection;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 @Path("/disziplin")
 public class DisziplinResource {
+	
+	/*@Inject
+	DataSource dataSource;*/
 
     @GET
     public String getPlainText(){
+    	
+    //	Connection connection = dataSource.getConnection();
+    	
         return "Hello world!";
     }
     
