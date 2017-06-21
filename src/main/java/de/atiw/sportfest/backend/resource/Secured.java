493d3d096604c1d77a141ff20cfbd7bca792edab
Tobias Daum <tobias.daum@s-atiw>
2017-06-21 15:24:27 +0200

    '#11 Authorisation test zeugs

---
+++ src/main/java/de/atiw/sportfest/backend/resource/Secured.java
@@ -0,0 +1,17 @@
+package de.atiw.sportfest.backend.resource;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.ws.rs.NameBinding;
+
+@NameBinding
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface Secured {
+	Role[] value() default {};
+}
