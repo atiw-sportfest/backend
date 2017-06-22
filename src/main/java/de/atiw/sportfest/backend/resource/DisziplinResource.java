c9ebb615fe053a1be5c5d55e4915ab72daf62d5b
christian.meinl <christian.meinl@Lab0320.s-atiw.de>
2017-06-22 09:35:08 +0200

    disziplin; disziplin resource

--- src/main/java/de/atiw/sportfest/backend/resource/DisziplinResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/DisziplinResource.java
@@ -1,7 +1,13 @@
 package de.atiw.sportfest.backend.resource;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -14,41 +20,56 @@ public class DisziplinResource {
 
 	ArrayList<Disziplin> dis = new ArrayList<Disziplin>(); 
 	
+	@Resource(name="jdbc/sportfest")
+    DataSource db;
 	
     @GET
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public ArrayList<Disziplin> getAlleDiziplinen(){
-          	Disziplin weit = new Disziplin();
-          	weit.setName("Weitsprung");
-          	weit.setBeschreibung("Springe so weit, wie du kannst!");
-          	weit.setMinTeilnehmer(1);
-          	weit.setMaxTeilnehmer(1);
-          	weit.setTeamleistung(false);
-          	Disziplin hoch = new Disziplin();
-          	hoch.setName("Hochsprung");
-          	hoch.setBeschreibung("Springe so hoch, wie du kannst!");
-          	hoch.setMinTeilnehmer(1);
-          	hoch.setMaxTeilnehmer(1);
-          	hoch.setTeamleistung(false);
-          	
-          	dis.add(weit);
-          	dis.add(hoch);
-          	
-          	return dis;
+        ArrayList<Disziplin> returner = new ArrayList<Disziplin>();
+    	try {
+			Connection connection = db.getConnection();
+			Statement stmt = connection.createStatement();
+			String query = "Call DisziplinenAnzeigen();  ";
+			ResultSet rs = stmt.executeQuery( query );
+			while(rs.next()){
+				returner.add(new Disziplin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getBoolean(6)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+      	return returner;
     }
     
     @GET
     @Path("/{did}")
-    public String getDiziplin(@PathParam("did") String did){
-          	
-    	return did;
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Disziplin getDiziplin(@PathParam("did") String did){
+    	try{
+    	Connection connection = db.getConnection();
+		Statement stmt = connection.createStatement();
+		String query = "Call disziplinanzeigen("+ Integer.parseInt(did)+");";
+		ResultSet rs = stmt.executeQuery( query );
+		if(rs.next())
+			return new Disziplin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getBoolean(6));
+		else 
+			return null;
+    	}catch(SQLException e){
+    		//Log Datei
+    		return null;
+    	}
+    	catch (Exception e) {
+    		//Parse
+    		//Log Datei
+			return null;
+		}
+		
     }
     
     
     @POST
     @Path("/{did}")
     public void postDiziplin(@PathParam("did") String did){
-    	//Disziplin.setName(did);
-
+    	
     }
 }
