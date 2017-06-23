0cf6105963ecd21b9ae64459cd36eca393ce971f
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-06-23 08:54:23 +0200

    User privileges

--- src/main/java/de/atiw/sportfest/backend/resource/UserResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/UserResource.java
@@ -1,18 +1,26 @@
 package de.atiw.sportfest.backend.resource;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+
+import de.atiw.sportfest.backend.auth.Secured;
 
 @Path("/user")
 public class UserResource {
 
 	
 	@GET
+    @Secured
 	@Path("/privileges")
-	public String getPrivileges(){
-		return "priv";
+    public Jws<Claims> getPrivileges(@HeaderParam("Authorization") String token){
+        return Jwts.parser().setSigningKey("secret".getBytes()).parseClaimsJws(token.substring("Bearer ".length()));
 	}
 	
 	@GET
@@ -36,7 +44,5 @@ public class UserResource {
 	public String delUser(){
 		return "tot";
 	}
-	
-	
 	
 }
