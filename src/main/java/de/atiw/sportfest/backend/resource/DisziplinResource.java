c8ece4f9d9e4235b78be8366cdf71712085e35f8
joham97 <joham97@web.de>
2017-06-24 17:21:48 +0200

    Rescourcen
    Disziplin Rescource:
    - Die Rückgabewerte sind immer Response (OK, INTERNAL_SERVER_ERROR,
    NOT_FOUND)
    - [GUEST] GET DISZIPLIN
    - [GUEST] GET DISZIPLIN/{did}
    - [ADMIN] PUT DISZIPLIN
    - [ADMIN] POST DISZIPLIN
    - [ADMIN] DELETE DISZIPLIN/{did}
    
    Klassen Rescource:
    - Die Rückgabewerte sind immer Response (OK, INTERNAL_SERVER_ERROR,
    NOT_FOUND)
    - [GUEST] GET KLASSE
    - [GUEST] GET KLASSE/{kid}
    - [ADMIN] PUT KLASSE
    - [ADMIN] DELETE KLASSE/{kid}
    
    Schüler Rescource:
    - Die Rückgabewerte sind immer Response (OK, INTERNAL_SERVER_ERROR,
    NOT_FOUND)
    - [GUEST] GET SCHUELER
    - [GUEST] GET SCHUELER/{sid}
    - [GUEST] GET SCHUELER/KLASSE/{kid}
    - [GUEST] GET SCHUELER/KLASSE/{kid}/DISZIPLIN/{did}
    - [ADMIN] PUT SCHUELER
    - [ADMIN] DELETE SCHUELER/{sid}

--- src/main/java/de/atiw/sportfest/backend/resource/DisziplinResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/DisziplinResource.java
@@ -9,13 +9,17 @@ import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import de.atiw.sportfest.backend.ExceptionResponse;
 import de.atiw.sportfest.backend.auth.Role;
 import de.atiw.sportfest.backend.auth.Secured;
 import de.atiw.sportfest.backend.resource.jaxb.Disziplin;
@@ -30,58 +34,124 @@ public class DisziplinResource {
 	
     @GET
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public ArrayList<Disziplin> getAlleDiziplinen(){
-        ArrayList<Disziplin> returner = new ArrayList<Disziplin>();
+    public Response getAlleDiziplinen(){
+        Response response = null; 
+		Connection connection = null;
     	try {		
-    		Connection connection = db.getConnection();
+    		connection = db.getConnection();
+    		ArrayList<Disziplin> returner = new ArrayList<Disziplin>();
 			ResultSet rs = Disziplin.getRSgetAll(connection);
 			while(rs.next()){
 				returner.add(new Disziplin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getBoolean(6), rs.getBoolean(7)));
 			}
-			connection.close();
+			response = Response.ok(returner).build();
 		} catch (SQLException e) {
 			e.printStackTrace();
+			response = ExceptionResponse.internalServerError(e);
+		}finally{
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-      	return returner;
+      	return response;
     }
     
     @GET
     @Path("/{did}")
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    public Disziplin getDiziplin(@PathParam("did") String did){
+    public Response getDiziplin(@PathParam("did") String did){
+        Response response = null; 
+		Connection connection = null;
 	    try{
-	    	Connection connection = db.getConnection();
+	    	connection = db.getConnection();
 			ResultSet rs = Disziplin.getRSgetOne(connection, did);
-			connection.close();
-		if(rs.next())
-			return new Disziplin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getBoolean(6), rs.getBoolean(7));
-		else 
-			return null;
-    	}catch(SQLException e){
-    		//Log Datei
-    		return null;
+			if(rs.next()){
+				response = Response.ok(new Disziplin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5), rs.getBoolean(6), rs.getBoolean(7))).build();
+			}else {
+				response = Response.status(Response.Status.NOT_FOUND).build();
+			}
     	}
     	catch (Exception e) {
-    		//Parse
-    		//Log Datei
-			return null;
+			response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
+	    return response;
 		
     }
     
     
     @PUT
     @Secured({ Role.admin })
-	@Path("/{did}")
 	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public boolean putDisziplin(Disziplin disziplin){
+	public Response putDisziplin(Disziplin disziplin){
+		Response response = null;
+		Connection connection = null;
     	try {
-    		Connection connection = db.getConnection();
+    		connection = db.getConnection();
 			Disziplin.getRSput(connection, disziplin);
-			connection.close();;
-			return true;
+			response = Response.ok().build();
     	} catch (SQLException e) {
-			return false;
+    		response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
+    	return response;
+    }
+    
+    @POST
+    @Secured({ Role.admin })
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response editDisziplin(Disziplin disziplin){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		connection = db.getConnection();
+			Disziplin.getRSpost(connection, disziplin);
+			response = Response.ok().build();
+    	} catch (SQLException e) {
+    		response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
+    }
+    
+    @DELETE
+    @Path("/{did}")
+    @Secured({ Role.admin })
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response deleteDisziplin(@PathParam("did") String did){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		connection = db.getConnection();
+			Disziplin.getRSdelete(connection, did);
+			connection.close();
+			response = Response.ok().build();
+    	} catch (SQLException e) {
+    		response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
     }
 }
--- src/main/java/de/atiw/sportfest/backend/resource/SchuelerResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/SchuelerResource.java
@@ -1,17 +1,178 @@
 package de.atiw.sportfest.backend.resource;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import de.atiw.sportfest.backend.ExceptionResponse;
 import de.atiw.sportfest.backend.auth.Role;
 import de.atiw.sportfest.backend.auth.Secured;
+import de.atiw.sportfest.backend.resource.jaxb.Schueler;
 
 @Path("/schueler")
 public class SchuelerResource{
+
+	@Resource(name="jdbc/sportfest")
+    DataSource db;
+	
+	@GET
+	@Path("/{sid}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response getSchueler(@PathParam("sid") String sid){
+		Response response = null;
+		Connection connection = null;
+		try{
+    		connection = db.getConnection();
+			ResultSet rs = Schueler.getRSgetOne(connection, sid);
+			if(rs.next()){
+				response = Response.ok(new Schueler(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5))).build();
+			}else{ 
+				response = Response.status(Response.Status.NOT_FOUND).build();
+			}
+    	} catch (Exception e) {
+			response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;			
+    }
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response getSchueler(){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		ArrayList<Schueler> returner = new ArrayList<Schueler>();
+    		connection = db.getConnection();
+			ResultSet rs = Schueler.getRSgetAll(connection);
+			while(rs.next()){
+				returner.add(new Schueler(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5)));
+			}
+			response = Response.ok(returner).build();
+		} catch (Exception e) {
+			response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
+	}
+	
+	@GET
+    @Path("/klasse/{kid}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response getSchuelerEinerKlasse(@PathParam("kid") String kid){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		ArrayList<Schueler> returner = new ArrayList<Schueler>();
+    		connection = db.getConnection();
+			ResultSet rs = Schueler.getRSgetAllOfKlasse(connection, kid);
+			while(rs.next()){
+				returner.add(new Schueler(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5)));
+			}
+			response = Response.ok(returner).build();
+		} catch (Exception e) {
+			response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
+	}
+	
+	@GET
+    @Path("/klasse/{kid}/disziplin/{did}")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response getSchuelerEinerKlasseUndDisziplin(@PathParam("kid") String kid, @PathParam("did") String did){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		ArrayList<Schueler> returner = new ArrayList<Schueler>();
+    		connection = db.getConnection();
+			ResultSet rs = Schueler.getRSgetAllOfKlasseAndDisziplin(connection, kid, did);
+			while(rs.next()){
+				returner.add(new Schueler(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5)));
+			}
+			response = Response.ok(returner).build();
+		} catch (Exception e) {
+			response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
+	}
+	
 	@PUT
     @Secured({ Role.admin })
-	public String putSchueler(){
-		return "putSchueler";
-	}
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response putSchueler(Schueler schueler){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		connection = db.getConnection();
+    		Schueler.getRSput(connection, schueler);
+			response = Response.ok().build();
+    	} catch (SQLException e) {
+    		response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+	    		response = ExceptionResponse.internalServerError(e);
+			}
+		}
+    	return response;
+    }
+    
+    @DELETE
+    @Path("/{sid}")
+    @Secured({ Role.admin })
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response deleteSchueler(@PathParam("sid") String sid){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		connection = db.getConnection();
+    		Schueler.getRSdelete(connection, sid);
+			response = Response.ok().build();
+    	} catch (SQLException e) {
+    		response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
+    }
 }
