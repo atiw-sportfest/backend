87beadc6ee190bb5ea75d064def602b5e8b2844b
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-06-25 18:40:11 +0200

    Remove test/token

--- src/main/java/de/atiw/sportfest/backend/resource/TestResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/TestResource.java
@@ -1,17 +1,11 @@
 package de.atiw.sportfest.backend.resource;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import de.atiw.sportfest.backend.auth.Secured;
 
@@ -32,14 +26,6 @@ public class TestResource {
     @Path("auth")
     public String checkAuth(@HeaderParam("Authorization") String token){
         return "Seems legit.\nYour token: " + token.substring("Bearer ".length());
-    }
-
-    @GET
-    @Secured
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @Path("token")
-    public Jws<Claims> checkToken(@HeaderParam("Authorization") String token){
-        return Jwts.parser().setSigningKey("secret".getBytes()).parseClaimsJws(token.substring("Bearer ".length()));
     }
 
     @GET
