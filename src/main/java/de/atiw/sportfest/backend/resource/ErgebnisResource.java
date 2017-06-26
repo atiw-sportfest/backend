3c42da23e9333657cf4c595a28b36138827ca410
christian.meinl <christian.meinl@Lab0320.s-atiw.de>
2017-06-26 10:53:39 +0200

    Ergebnis db Connection

--- src/main/java/de/atiw/sportfest/backend/resource/ErgebnisResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/ErgebnisResource.java
@@ -1,44 +1,142 @@
 package de.atiw.sportfest.backend.resource;
 
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import de.atiw.sportfest.backend.ExceptionResponse;
 import de.atiw.sportfest.backend.auth.Role;
 import de.atiw.sportfest.backend.auth.Secured;
+import de.atiw.sportfest.backend.resource.jaxb.Disziplin;
+import de.atiw.sportfest.backend.resource.jaxb.Ergebnis;
 
 @Path("/ergebnis")
 public class ErgebnisResource {
+	
+	@Resource(name="jdbc/sportfest")
+    DataSource db;
+	
+	@GET
+    public Response getErgebnisse(){
+		Response response = null; 
+		Connection connection = null;
+    	try {		
+    		connection = db.getConnection();
+			response = Response.ok(Ergebnis.getAll(connection)).build();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			response = ExceptionResponse.internalServerError(e);
+		}finally{
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+      	return response;
+    }
 
 	@GET
-	@Path("/{did}")
-    public String getErgebnis(@PathParam("did") String did){
-        return "Die gewählte Disziplin ist: "+did;
+	@Path("/{eid}")
+    public Response getErgebnis(@PathParam("eid") String eid){
+		Response response = null; 
+		Connection connection = null;
+    	try {		
+    		connection = db.getConnection();
+			response = Response.ok(Ergebnis.getOne(connection, eid)).build();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			response = ExceptionResponse.internalServerError(e);
+		}finally{
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+      	return response;
     }
 	
 		
 	@POST
     @Secured({ Role.admin })
-	@Path("/{did}/{eid}")
-	public String postErgenis(@PathParam("did") String did, @PathParam("eid") String eid){
-		return "PUT "+did+", "+eid;
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Response postErgenis(Ergebnis ergebnis){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		connection = db.getConnection();
+			Ergebnis.post(connection, ergebnis);
+			response = Response.ok().build();
+    	} catch (SQLException e) {
+    		response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
 	}
 	
 	@PUT
     @Secured({ Role.admin })
-	@Path("/{did}")
-	 public String putErgebnis(@PathParam("did") String did){
-		return "PUT  "+ did;
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	 public Response putErgebnis(Ergebnis ergebnis){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		connection = db.getConnection();
+			Ergebnis.put(connection, ergebnis);
+			response = Response.ok().build();
+    	} catch (SQLException e) {
+    		response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
     }
 	
 	@DELETE
     @Secured({ Role.admin })
-	@Path("/{did}/{eid}")
-	 public String deleteErgebnis(@PathParam("did") String did, @PathParam("eid") String eid){
-		return "DELETE "+ eid;
+	@Path("/{eid}")
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	 public Response deleteErgebnis( @PathParam("eid") String eid){
+		Response response = null;
+		Connection connection = null;
+    	try {
+    		connection = db.getConnection();
+			Ergebnis.delete(connection, eid);
+			connection.close();
+			response = Response.ok().build();
+    	} catch (SQLException e) {
+    		response = ExceptionResponse.internalServerError(e);
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return response;
     }
 }
--- src/main/java/de/atiw/sportfest/backend/resource/jaxb/Ergebnis.java
+++ src/main/java/de/atiw/sportfest/backend/resource/jaxb/Ergebnis.java
@@ -1,5 +1,11 @@
 package de.atiw.sportfest.backend.resource.jaxb;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Ergebnis {
 
 	private int eid;
@@ -8,18 +14,16 @@ public class Ergebnis {
 	private String beschreibung;
 	private String variablenname;
 	private int bewid;
-	private int istzustand;
 	
 	
 	
-	public Ergebnis(int eid, int did, String name, String beschreibung, String variablenname, int bewid, int istzustand){
+	public Ergebnis(int eid, int did, String name, String beschreibung, String variablenname, int bewid){
 		this.eid = eid;
 		this.did = did;
 		this.name = name;
 		this.beschreibung = beschreibung;
 		this.variablenname = variablenname;
 		this.bewid = bewid;
-		this.istzustand = istzustand;
 	}
 	
 	public int getEid() {
@@ -58,10 +62,57 @@ public class Ergebnis {
 	public void setBewid(int bewid) {
 		this.bewid = bewid;
 	}
-	public int getIstzustand() {
-		return istzustand;
+	
+	public static ResultSet getRSgetAll(Connection conn) throws SQLException{		
+		return conn.prepareStatement("Call ErgebnisseAnzeigen(); ").executeQuery();
+		
 	}
-	public void setIstzustand(int istzustand) {
-		this.istzustand = istzustand;
-	}	
+	public static ResultSet getRSgetOne(Connection conn, String eid) throws SQLException{			 
+		PreparedStatement ps = conn.prepareStatement("Call ErgebnisAnzeigen(?)");
+		ps.setString(1, eid);
+		return ps.executeQuery();
+	}
+	public static void put(Connection conn, Ergebnis ergebnis) throws SQLException{	
+		PreparedStatement ps = conn.prepareStatement("Call ErgebnisAnlegen(?,?,?,?,?)");
+		ps.setInt(1,ergebnis.getDid() );
+		ps.setString(2,ergebnis.getName());
+		ps.setString(3,ergebnis.getBeschreibung());
+		ps.setString(4,ergebnis.getVariablenname());
+		ps.setInt(5,ergebnis.getBewid());
+		ps.execute();
+	}
+	public static void post(Connection conn, Ergebnis ergebnis) throws SQLException{	
+		PreparedStatement ps = conn.prepareStatement("Call ErgebnisAendern (?,?,?,?,?,?)");
+		ps.setInt(1,ergebnis.getEid() );
+		ps.setInt(2,ergebnis.getDid() );
+		ps.setString(3,ergebnis.getName());
+		ps.setString(4,ergebnis.getBeschreibung());
+		ps.setString(5,ergebnis.getVariablenname());
+		ps.setInt(6,ergebnis.getBewid());
+		ps.execute();
+	}
+	public static void delete(Connection conn, String eid) throws SQLException{	
+		PreparedStatement ps = conn.prepareStatement("Call ErgebnisLoeschen(?)");
+		ps.setString(1,eid);
+		ps.execute();
+	}
+
+	public static Ergebnis getOne(Connection conn, String did) throws SQLException{
+		ResultSet rs;
+		rs = getRSgetOne(conn, did);
+		if(rs.next()){
+			return new Ergebnis(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6));
+		}
+		return null;
+	}
+	
+	public static ArrayList<Ergebnis> getAll(Connection conn) throws SQLException{
+		ResultSet rs;
+		rs = getRSgetAll(conn);
+		ArrayList<Ergebnis> returner = new ArrayList<>();
+		while(rs.next()){
+			returner.add(new Ergebnis(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6)));
+		}
+		return returner;
+	}
 }
