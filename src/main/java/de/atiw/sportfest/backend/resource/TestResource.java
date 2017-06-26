1a94d3b140026db4afb8b4d63713a9f70a16f0ef
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-06-27 00:49:21 +0200

    @XmlElement's test in TestResource

--- src/main/java/de/atiw/sportfest/backend/resource/TestResource.java
+++ src/main/java/de/atiw/sportfest/backend/resource/TestResource.java
@@ -8,8 +8,10 @@ import javax.sql.DataSource;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -124,10 +126,18 @@ public class TestResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Path("value")
     public Response testValues(){
         return Response.ok(new ValueTest()).build();
     }
 
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("value")
+    public Response testValues(ValueTest v){
+        return Response.ok(v).build();
+    }
 }
 
 @XmlRootElement
