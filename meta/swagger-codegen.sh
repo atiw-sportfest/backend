9b14fd89517af8edcbe43bb532ac5a7614d8b3ee
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-11-28 20:27:58 +0100

    Update swagger-codegen.sh

--- meta/swagger-codegen.sh
+++ meta/swagger-codegen.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+dir=$(dirname $0)
+
 genlang="jaxrs-spec"
 ziptmp=$(mktemp)
 dirtmp=$(mktemp -d)
-tgtdir=$(cd .. && pwd)
+tgtdir=$(cd $dir/.. && pwd)
 
 if [ ! -z "$SWAGGER_CODEGEN_JAR" ]; then
     echo >&2 "Not yet supported."
     #java -jar "$SWAGGER_CODEGEN_JAR" generate -i swagger.yaml -l $genlang -o ../src/main/java -c swagger-codegen.config.json
 else
-    options=$(jq ".[0].spec = .[1] | .[0]" -s swagger-codegen.config.json swagger.json) # Embed spec in options
+    options=$(jq ".[0].spec = .[1] | .[0]" -s $dir/swagger-codegen.config.json $dir/swagger.json) # Embed spec in options
     response=$(curl -k https://generator.swagger.io/api/gen/servers/${genlang} -d "$options" -H "Content-Type: application/json")
 
     link=$(echo $response | jq ".link" -r)
