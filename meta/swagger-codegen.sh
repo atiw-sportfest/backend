fe90b6a397947222acd6167bf44767f152629e17
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-01 16:14:49 +0100

    swagger-codegen local

--- meta/swagger-codegen.sh
+++ meta/swagger-codegen.sh
@@ -7,9 +7,14 @@ ziptmp=$(mktemp)
 dirtmp=$(mktemp -d)
 tgtdir=$(cd $dir/../src/main/java && pwd)
 
-if [ ! -z "$SWAGGER_CODEGEN_JAR" ]; then
-    echo >&2 "Not yet supported."
-    #java -jar "$SWAGGER_CODEGEN_JAR" generate -i swagger.yaml -l $genlang -o ../src/main/java -c swagger-codegen.config.json
+if [ ! -z "$SWAGGER_CODEGEN" ]; then
+
+    java -jar "$SWAGGER_CODEGEN" generate -i $dir/swagger.json -l $genlang -c $dir/swagger-codegen.config.json -o $tgtdir
+
+    pushd $tgtdir
+
+    rm -rf .swagger-codegen* swagger.json pom.xml
+
 else
     options=$(jq -s '{ options: .[0], spec: .[1] }' $dir/swagger{-codegen.config,}.json)
     response=$(curl -k https://generator.swagger.io/api/gen/servers/${genlang} -d "$options" -H "Content-Type: application/json")
@@ -35,6 +40,6 @@ else
 
     cd $tgtdir
 
-    git commit -m "Update generated Model and Api" .
-
 fi
+
+git commit -m "Update generated Model and Api" .
