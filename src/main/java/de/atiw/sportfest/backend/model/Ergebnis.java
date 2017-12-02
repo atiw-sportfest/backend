8d365ca360a6d852ced9c31ce821878ab0845888
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-02 19:04:39 +0100

    KlasseApi

--- src/main/java/de/atiw/sportfest/backend/model/Ergebnis.java
+++ src/main/java/de/atiw/sportfest/backend/model/Ergebnis.java
@@ -25,7 +25,8 @@ import javax.validation.constraints.*;
 @NamedQueries({
 @NamedQuery(name="ergebnis.listByDisziplin", query="SELECT e FROM Ergebnis e JOIN e.disziplin d JOIN FETCH e.leistungen WHERE d.id = :did"),
 @NamedQuery(name="ergebnis.listByDisziplinAndKlasse", query="SELECT e FROM Ergebnis e JOIN e.disziplin d JOIN e.klasse k JOIN FETCH e.leistungen WHERE d.id = :did AND k.id = :kid"),
-@NamedQuery(name="ergebnis.listByDisziplinAndSchueler", query="SELECT e FROM Ergebnis e JOIN e.disziplin d JOIN e.schueler s JOIN FETCH e.leistungen WHERE d.id = :did AND s.id = :sid")
+@NamedQuery(name="ergebnis.listByDisziplinAndSchueler", query="SELECT e FROM Ergebnis e JOIN e.disziplin d JOIN e.schueler s JOIN FETCH e.leistungen WHERE d.id = :did AND s.id = :sid"),
+@NamedQuery(name="ergebnis.listByKlasse", query="SELECT e FROM Ergebnis e JOIN e.klasse k JOIN FETCH e.leistungen WHERE k.id = :kid")
 })
 public class Ergebnis   {
 
