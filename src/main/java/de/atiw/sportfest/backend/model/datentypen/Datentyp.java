4fcbb42e51fed8fca7d77fda8749047ac4fd3c24
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-03 20:49:25 +0100

    Remove Datentyp#save

--- src/main/java/de/atiw/sportfest/backend/model/datentypen/Datentyp.java
+++ src/main/java/de/atiw/sportfest/backend/model/datentypen/Datentyp.java
@@ -4,7 +4,5 @@ public interface Datentyp<T> {
 
   public T load(String s);
 
-  public String save(T t);
-
 }
 
--- src/main/java/de/atiw/sportfest/backend/model/datentypen/StringTyp.java
+++ src/main/java/de/atiw/sportfest/backend/model/datentypen/StringTyp.java
@@ -2,7 +2,6 @@ package de.atiw.sportfest.backend.model.datentypen;
 
 public class StringTyp implements Datentyp<String> {
 
-    public String save(String s){ return s; }
     public String load(String s){ return s; }
 
 }
