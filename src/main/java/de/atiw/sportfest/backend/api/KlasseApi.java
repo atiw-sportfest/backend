ba3cc604d6ede02524c2357e2f31e37654aa03f2
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-11 00:19:25 +0100

    Upload Anmeldungen

--- src/main/java/de/atiw/sportfest/backend/api/KlasseApi.java
+++ src/main/java/de/atiw/sportfest/backend/api/KlasseApi.java
@@ -1,24 +1,37 @@
 package de.atiw.sportfest.backend.api;
 
+import excel.exports.DBToExcelDisziplin;
+import excel.exports.DBToExcelExporter;
+import excel.exports.DBToExcelSchueler;
+import excel.imports.ExcelToDBImporter;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 
 import de.atiw.sportfest.backend.model.Anmeldung;
+import de.atiw.sportfest.backend.model.Disziplin;
 import de.atiw.sportfest.backend.model.Ergebnis;
-import java.io.File;
-import java.io.InputStream;
 import de.atiw.sportfest.backend.model.Klasse;
 import de.atiw.sportfest.backend.model.Schueler;
 import io.swagger.annotations.*;
 import javax.validation.constraints.*;
 import javax.ws.rs.*;
-
-import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 
 @Path("/klasse")
 
@@ -50,7 +63,25 @@ public class KlasseApi  {
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "Excel-Anmeldebogen", response = byte[].class) })
     public Response klasseKidAnmeldebogenGet(@PathParam("kid") @ApiParam("Klassen-ID") Long kid) {
-        return Response.ok().entity("magic!").build();
+
+        List<DBToExcelDisziplin> disz = em.createNamedQuery("disziplin.list", Disziplin.class).getResultList().stream()
+            .map(d -> new DBToExcelDisziplin( d.getBezeichnung(), d.getId().intValue(), d.getTeam() == null ? false : d.getTeam(), 0, 5)) //TODO: Disziplin.{min,max}
+            .collect(Collectors.toList());
+
+        List<DBToExcelSchueler> schueler = em.createNamedQuery("schueler.listByKlasse", Schueler.class).setParameter("kid", kid).getResultList().stream()
+            .map(s -> new DBToExcelSchueler(s.getNachname(), s.getNachname(), s.getId().intValue()))
+            .collect(Collectors.toList());
+
+        Klasse k = em.find(Klasse.class, kid);
+
+        StreamingOutput stream = new StreamingOutput(){
+            public void write(OutputStream os) throws IOException, WebApplicationException {
+                DBToExcelExporter.export(os, k.getBezeichnung(), schueler, disz);
+                os.flush();
+            }
+        };
+
+        return Response.ok().entity(stream).header("Content-Disposition", String.format("attachment; filename=%s.xlsx", k.getBezeichnung())).build();
     }
 
     @POST
@@ -60,9 +91,16 @@ public class KlasseApi  {
     @ApiOperation(value = "Anmeldebogen für eine Klasse hochladen", notes = "", response = void.class, tags={ "Anmeldung",  })
     @ApiResponses(value = { 
         @ApiResponse(code = 204, message = "Anmeldungen erstellt", response = void.class) })
-    public Response klasseKidAnmeldebogenPost(@PathParam("kid") @ApiParam("Klassen-ID") Long kid, @FormParam(value = "file") InputStream fileInputStream,
-   @FormParam(value = "file") Attachment fileDetail) {
-        return Response.ok().entity("magic!").build();
+    public Response klasseKidAnmeldebogenPost(@PathParam("kid") @ApiParam("Klassen-ID") Long kid, @Multipart(value = "file") InputStream fileInputStream,
+   @Multipart(value = "file") Attachment fileDetail) throws IOException {
+
+        ExcelToDBImporter.importTeilnahmen(fileInputStream).stream().forEach(t ->
+                em.merge(new Anmeldung()
+                    .disziplin(new Disziplin().id(new Integer(t.getDisziplinID()).longValue()))
+                    .schueler(new Schueler().id(new Integer(t.getSchuelerID()).longValue())))
+                );
+
+        return Response.noContent().build();
     }
 
     @GET
