7a4e0977f54815c50d4a7755abfa8ca9f586fdf7
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-11 19:17:18 +0100

    Update generated Model and Api

---
+++ src/main/java/de/atiw/sportfest/backend/api/SportfestApi.java
@@ -0,0 +1,43 @@
+package de.atiw.sportfest.backend.api;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import io.swagger.annotations.*;
+
+import java.util.List;
+import javax.validation.constraints.*;
+
+@Path("/sportfest")
+
+@Api(description = "the sportfest API")
+
+
+
+
+public class SportfestApi  {
+
+    @DELETE
+    
+    
+    
+    @ApiOperation(value = "", notes = "Sportfest zurücksetzen", response = void.class, tags={ "Meta",  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 204, message = "Sportfest zurückgesetzt", response = void.class) })
+    public Response sportfestDelete() {
+        return Response.ok().entity("magic!").build();
+    }
+
+    @POST
+    
+    
+    
+    @ApiOperation(value = "", notes = "Sportfest beenden", response = void.class, tags={ "Meta" })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 204, message = "Sportfest beendet", response = void.class) })
+    public Response sportfestPost() {
+        return Response.ok().entity("magic!").build();
+    }
+}
+
