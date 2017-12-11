a3617e4dc305dea84bfecc7adda148ec89c87d83
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-11 23:10:08 +0100

    DSL-Validation

--- src/main/java/de/atiw/sportfest/backend/api/DslApi.java
+++ src/main/java/de/atiw/sportfest/backend/api/DslApi.java
@@ -1,14 +1,15 @@
 package de.atiw.sportfest.backend.api;
 
-import de.atiw.sportfest.backend.model.ValidationResult;
-
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
-
-import io.swagger.annotations.*;
+import groovy.lang.GroovyShell;
 
 import java.util.List;
+
+import javax.ws.rs.core.Response;
+
+import de.atiw.sportfest.backend.model.ValidationResult;
+import io.swagger.annotations.*;
 import javax.validation.constraints.*;
+import javax.ws.rs.*;
 
 @Path("/dsl")
 
@@ -26,8 +27,14 @@ public class DslApi  {
     @ApiOperation(value = "", notes = "Syntax-Prüfung der Regel-DSL", response = ValidationResult.class, tags={ "Meta" })
     @ApiResponses(value = { 
         @ApiResponse(code = 200, message = "Ergebniss der Syntaxprüfung", response = ValidationResult.class) })
-    public Response dslCheckRegelPost(String script) {
-        return Response.ok().entity("magic!").build();
+    public ValidationResult dslCheckRegelPost(String script) {
+
+        try {
+            new GroovyShell().parse(script);
+            return new ValidationResult().pass(true);
+        } catch (Exception e){
+            return new ValidationResult().pass(false).messages(e.getMessage());
+        }
     }
 }
 
