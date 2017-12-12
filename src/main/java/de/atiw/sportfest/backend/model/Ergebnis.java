c7ce13e286f8c4dff63a315238cd2bd1452fa7e9
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-12 19:24:27 +0100

    Alle Ergebnisse auflisten

--- src/main/java/de/atiw/sportfest/backend/model/Ergebnis.java
+++ src/main/java/de/atiw/sportfest/backend/model/Ergebnis.java
@@ -24,6 +24,7 @@ import javax.validation.constraints.*;
 
 @Entity
 @NamedQueries({
+@NamedQuery(name="ergebnis.list", query="SELECT e FROM Ergebnis e"),
 @NamedQuery(name="ergebnis.listByDisziplin", query="SELECT e FROM Ergebnis e JOIN e.disziplin d JOIN FETCH e.leistungen WHERE d.id = :did"),
 @NamedQuery(name="ergebnis.listByDisziplinAndKlasse", query="SELECT e FROM Ergebnis e JOIN e.disziplin d JOIN e.klasse k JOIN FETCH e.leistungen WHERE d.id = :did AND k.id = :kid"),
 @NamedQuery(name="ergebnis.listByDisziplinAndSchueler", query="SELECT e FROM Ergebnis e JOIN e.disziplin d JOIN e.schueler s JOIN FETCH e.leistungen WHERE d.id = :did AND s.id = :sid"),
